bc7f039b233d8f4a95a267eaa91044b6748624bc
SkoobyDoo <theskoobydoo@gmail.com>
2021-04-24 23:23:14 -0700

    Implement dynamic camera zoom based on 0th tracking object velocity magnitude

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,6 +6,13 @@ public class CameraFollow : MonoBehaviour
 {
 	public GameObject m_camera;
 	public List<GameObject> m_cameraFollowTargets = new List<GameObject>();
+	public float m_minCameraSize = 5f;
+	public float m_maxCameraSize = 50f;
+	public float m_minSpeed = 1f;
+	public float m_maxSpeed = 30f;
+	[Range(0, .5f)] public float m_cameraZoomSmoothing = .3f;
+	
+	private float camVelocity = 0f;
 	
     // Start is called before the first frame update
     void Start()
@@ -25,6 +32,12 @@ public class CameraFollow : MonoBehaviour
 		
 		
         m_camera.transform.position = new Vector3(tempSum.x, tempSum.y, m_camera.transform.position.z);
+		
+		//float speedRatio = (Mathf.Clamp(m_cameraFollowTargets[0].GetComponent<Rigidbody2D>().velocity, m_minSpeed, m_maxSpeed) - m_minSpeed) / (m_maxSpeed - m_minSpeed);
+		float speedRatio = Mathf.InverseLerp(m_minSpeed,m_maxSpeed,m_cameraFollowTargets[0].GetComponent<Rigidbody2D>().velocity.magnitude);
+		float targetZoom = Mathf.Lerp(m_minCameraSize,m_maxCameraSize,speedRatio);
+		Camera cam = m_camera.GetComponent<Camera>();
+		cam.orthographicSize = Mathf.SmoothDamp(cam.orthographicSize, targetZoom, ref camVelocity, m_cameraZoomSmoothing);
     }
 	
 }
