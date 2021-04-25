cb3eae5285dfb041075a85a8e619debc1bf95254
SkoobyDoo <theskoobydoo@gmail.com>
2021-04-25 15:03:48 -0700

    Reparent some scene objects

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -22,7 +22,7 @@ public class CameraFollow : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+		
     }
 
     // Update is called once per frame
