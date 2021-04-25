6f615755103740ac6d5177ebd3c4aa6a5ea0d692
SkoobyDoo <theskoobydoo@gmail.com>
2021-04-25 10:22:36 -0700

    Implement smoothdamp camera following

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,13 +6,15 @@ public class CameraFollow : MonoBehaviour
 {
 	public GameObject m_camera;
 	public List<GameObject> m_cameraFollowTargets = new List<GameObject>();
+	[Range(0, .5f)] public float m_cameraPosSmoothing = .5f;
 	public float m_minCameraSize = 5f;
 	public float m_maxCameraSize = 50f;
 	public float m_minSpeed = 1f;
 	public float m_maxSpeed = 30f;
 	[Range(0, .5f)] public float m_cameraZoomSmoothing = .3f;
 	
-	private float camVelocity = 0f;
+	private Vector3 camVelocity = new Vector3();
+	private float camZoomVelocity = 0f;
 	
     // Start is called before the first frame update
     void Start()
@@ -23,21 +25,22 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		Vector3 tempSum = new Vector3();
+		Vector3 targetPos = new Vector3();
 		foreach (var x in m_cameraFollowTargets)
 		{
-			tempSum += x.transform.position;
+			targetPos += x.transform.position;
 		}
-		tempSum /= m_cameraFollowTargets.Count;
+		targetPos /= m_cameraFollowTargets.Count;
+		targetPos.z = m_camera.transform.position.z;
 		
-		
-        m_camera.transform.position = new Vector3(tempSum.x, tempSum.y, m_camera.transform.position.z);
+		Vector3 currentPos = m_camera.transform.position;
+        m_camera.transform.position = Vector3.SmoothDamp(currentPos, targetPos, ref camVelocity, m_cameraPosSmoothing);
 		
 		//float speedRatio = (Mathf.Clamp(m_cameraFollowTargets[0].GetComponent<Rigidbody2D>().velocity, m_minSpeed, m_maxSpeed) - m_minSpeed) / (m_maxSpeed - m_minSpeed);
 		float speedRatio = Mathf.InverseLerp(m_minSpeed,m_maxSpeed,m_cameraFollowTargets[0].GetComponent<Rigidbody2D>().velocity.magnitude);
 		float targetZoom = Mathf.Lerp(m_minCameraSize,m_maxCameraSize,speedRatio);
 		Camera cam = m_camera.GetComponent<Camera>();
-		cam.orthographicSize = Mathf.SmoothDamp(cam.orthographicSize, targetZoom, ref camVelocity, m_cameraZoomSmoothing);
+		cam.orthographicSize = Mathf.SmoothDamp(cam.orthographicSize, targetZoom, ref camZoomVelocity, m_cameraZoomSmoothing);
     }
 	
 }
